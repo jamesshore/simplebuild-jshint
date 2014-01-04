74fca50d7dbe5d9b8c428e21505da5199dd442a9
James Shore <jshore@jamesshore.com>
2014-01-04 16:23:48 -0600

    Tested that validator fails successfully

--- src/_simplebuild-jshint_test.js
+++ src/_simplebuild-jshint_test.js
@@ -6,35 +6,34 @@ var jshint = require("./simplebuild-jshint.js");
 
 describe("Simplebuild module", function() {
 
+	var successCalled;
+	var failureCalled;
+
+	beforeEach(function() {
+		successCalled = false;
+		failureCalled = false;
+	});
+
 	describe("source validator", function() {
 
 		//TODO: factor out and use console inspector?
 		//TODO: simplify testing of success and failure functions
 		//TODO: fix console inspector; it's squashing output permanently
-
-		var successCalled;
-		var failureCalled;
-
-		beforeEach(function() {
-			successCalled = false;
-			failureCalled = false;
-		});
+		//TODO: validate options
 
 		it("calls success() callback on success", function() {
 			jshint.checkSource({
-//				code: "var a = 1;"
+				code: "var a = 1;"
 			}, success, failure);
 			expectSuccess();
 		});
 
-//		it("calls failure() callback on failure", function() {
-//			jshint.checkSource({
-//				code: "bargledy-bargle"
-//			}, success, failure);
-//
-//			expect(successCalled).to.be(false);
-//			expect(failureCalled).to.be(true);
-//		});
+		it("calls failure() callback on failure", function() {
+			jshint.checkSource({
+				code: "bargledy-bargle"
+			}, success, failure);
+			expectFailure();
+		});
 
 //		it("takes source code, options, and globals", function() {
 //			jshint.checkSource({
@@ -46,28 +45,30 @@ describe("Simplebuild module", function() {
 //			expect(succeeded).to.be(true);
 //
 //		});
-
-		function success() {
-			successCalled = true;
-		}
-
-		function failure() {
-			failureCalled = false;
-		}
-
-		function expectSuccess() {
-			if (!successCalled) throw new Error("Expected success callback to be called");
-			if (failureCalled) throw new Error("Did not expect failure callback to be called");
-		}
-
-		function expectFailure() {
-			if (!failureCalled) throw new Error("Expected failure callback to be called");
-			if (successCalled) throw new Error("Did not expect success callback to be called");
-		}
 	});
 
 	describe("file validator", function() {
 		//TBD
 	});
+
+
+
+	function success() {
+		successCalled = true;
+	}
+
+	function failure() {
+		failureCalled = true;
+	}
+
+	function expectSuccess() {
+		if (!successCalled) throw new Error("Expected success callback to be called");
+		if (failureCalled) throw new Error("Did not expect failure callback to be called");
+	}
+
+	function expectFailure() {
+		if (!failureCalled) throw new Error("Expected failure callback to be called");
+		if (successCalled) throw new Error("Did not expect success callback to be called");
+	}
 
 });
--- src/simplebuild-jshint.js
+++ src/simplebuild-jshint.js
@@ -1,6 +1,10 @@
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 "use strict";
 
+var jshint = require("./jshint_runner.js");
+
 exports.checkSource = function checkSource(options, success, failure) {
-	success();
+	var passed = jshint.validateSource(options.code);
+	if (passed) success();
+	else failure();
 };
